8375006ea4cde24205570a4fe5661e34b86d82cd
Acacio Guirra <acacioguirra@gmail.com>
2024-08-10 17:24:18 -0300

    base do codigo

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        int numero;
-        String agencia;
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
         
+        System.out.println("Por favor, digite o seu nome:");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o numero da sua conta: ");        
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o valor do seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+
+        
+        
+        
+
+
     }
 }
